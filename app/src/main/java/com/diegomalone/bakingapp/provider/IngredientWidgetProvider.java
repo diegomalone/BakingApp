123bb239a9753f5eebac8e9f8930e8a55c1e413a
Diego Malone <diegomalone@gmail.com>
2018-02-17 15:20:16 -0200

    Starting to create widget

---
+++ app/src/main/java/com/diegomalone/bakingapp/provider/IngredientWidgetProvider.java
@@ -0,0 +1,50 @@
+package com.diegomalone.bakingapp.provider;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.diegomalone.bakingapp.R;
+import com.diegomalone.bakingapp.ui.activity.RecipeListActivity;
+
+/**
+ * Created by malone on 13/02/18.
+ */
+
+public class IngredientWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        Intent intent = new Intent(context, RecipeListActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_ingredient);
+        views.setOnClickPendingIntent(R.id.widget_ingredient_image, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // Perform any action when one or more AppWidget instances have been deleted
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Perform any action when an AppWidget for this provider is instantiated
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Perform any action when the last AppWidget instance for this provider is deleted
+    }
+
+}
