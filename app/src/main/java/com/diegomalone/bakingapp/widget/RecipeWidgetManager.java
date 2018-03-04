2f3915e4cd43d481fcbfeff80c4f55535bb4fb78
Diego Malone <diegomalone@gmail.com>
2018-03-04 20:16:53 -0300

    Creating widget structure

---
+++ app/src/main/java/com/diegomalone/bakingapp/widget/RecipeWidgetManager.java
@@ -0,0 +1,50 @@
+package com.diegomalone.bakingapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.diegomalone.bakingapp.R;
+import com.diegomalone.bakingapp.model.Recipe;
+import com.google.gson.Gson;
+
+/**
+ * Created by malone on 25/02/18.
+ */
+
+public class RecipeWidgetManager {
+
+    private static final String PREFS_FILE_NAME = "baking_app";
+    private static final String KEY_WIDGET_RECIPE_ID = "recipeId";
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+
+    public RecipeWidgetManager(Context context) {
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(PREFS_FILE_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void setRecipe(Recipe recipe) {
+        String recipeJson = new Gson().toJson(recipe);
+
+        sharedPreferences.edit().putString(KEY_WIDGET_RECIPE_ID, recipeJson).apply();
+
+        updateWidget();
+    }
+
+    private void updateWidget() {
+        Intent intent = new Intent(context, RecipeWidgetProvider.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName componentName = new ComponentName(context, RecipeWidgetProvider.class);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(componentName);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+        context.sendBroadcast(intent);
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_ingredient_list);
+    }
+}
--- app/src/main/java/com/diegomalone/bakingapp/widget/RecipeWidgetProvider.java
+++ app/src/main/java/com/diegomalone/bakingapp/widget/RecipeWidgetProvider.java
@@ -1,4 +1,4 @@
-package com.diegomalone.bakingapp.provider;
+package com.diegomalone.bakingapp.widget;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -14,14 +14,14 @@ import com.diegomalone.bakingapp.ui.activity.RecipeListActivity;
  * Created by malone on 13/02/18.
  */
 
-public class IngredientWidgetProvider extends AppWidgetProvider {
+public class RecipeWidgetProvider extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
         Intent intent = new Intent(context, RecipeListActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_ingredient);
-        views.setOnClickPendingIntent(R.id.widget_ingredient_image, pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_ingredient_list, pendingIntent);
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
@@ -30,21 +30,6 @@ public class IngredientWidgetProvider extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-    }
-
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        // Perform any action when one or more AppWidget instances have been deleted
-    }
-
-    @Override
-    public void onEnabled(Context context) {
-        // Perform any action when an AppWidget for this provider is instantiated
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Perform any action when the last AppWidget instance for this provider is deleted
     }
 
 }
